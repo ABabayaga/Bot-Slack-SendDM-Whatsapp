2c95349c2b7585d6ff5e36a794eada8c2a51705b
ABabayaga <alef_rdl@hotmail.com>
2025-08-20 16:33:48 -0400

    AJuste port

--- slack-userdm-to-whatsapp.cjs
+++ slack-userdm-to-whatsapp.cjs
@@ -357,3 +357,9 @@ async function pollOnce() {
   }
 })();
 
+const http = require('http');
+const port = process.env.PORT || 3000;
+http.createServer((_,res)=>{res.writeHead(200);res.end('ok');})
+    .listen(port, ()=>console.log('health server on', port));
+
+
